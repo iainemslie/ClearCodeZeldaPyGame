5977828903f608184bf29c66ba20827bb85a2b6a
Iain Emslie <iainemslie@gmail.com>
2024-02-21 13:15:41 -0500

    add weapon switching to ui

--- code/player.py
+++ code/player.py
@@ -94,9 +94,13 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_q] and self.can_switch_weapon:
                 self.can_switch_weapon = False
                 self.weapon_switch_time = pygame.time.get_ticks()
-                self.weapon_index += 1
-                self.weapon = list(weapon_data.keys())[
-                    self.weapon_index % len(weapon_data)]
+
+                if self.weapon_index < len(list(weapon_data.keys())) - 1:
+                    self.weapon_index += 1
+                else:
+                    self.weapon_index = 0
+
+            self.weapon = list(weapon_data.keys())[self.weapon_index]
 
     def get_status(self):
         # idle status
