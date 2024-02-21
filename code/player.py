26f80de5b2d8da59a1aabe0351ff7e8d89e7d8d1
Iain Emslie <iainemslie@gmail.com>
2024-02-21 11:10:54 -0500

    added weapon cycling

--- code/player.py
+++ code/player.py
@@ -29,9 +29,12 @@ class Player(pygame.sprite.Sprite):
 
         # weapon
         self.create_attack = create_attack
+        self.destroy_attack = destroy_attack
         self.weapon_index = 0
         self.weapon = list(weapon_data.keys())[self.weapon_index]
-        self.destroy_attack = destroy_attack
+        self.can_switch_weapon = True
+        self.weapon_switch_time = None
+        self.switch_duration_cooldown = 200
 
     def import_player_assets(self):
         character_path = 'graphics/player/'
@@ -79,6 +82,13 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 print("ALAKAZAM")
+
+            if keys[pygame.K_q] and self.can_switch_weapon:
+                self.can_switch_weapon = False
+                self.weapon_switch_time = pygame.time.get_ticks()
+                self.weapon_index += 1
+                self.weapon = list(weapon_data.keys())[
+                    self.weapon_index % len(weapon_data)]
 
     def get_status(self):
         # idle status
@@ -132,6 +142,10 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.attack_time > self.attack_cooldown:
                 self.attacking = False
                 self.destroy_attack()
+
+        if not self.can_switch_weapon:
+            if current_time - self.weapon_switch_time > self.switch_duration_cooldown:
+                self.can_switch_weapon = True
 
     def animate(self):
         animation = self.animations[self.status]
